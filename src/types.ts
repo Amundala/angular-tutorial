a89a520c5f95b731395e59dcdf0466366a3f6006
Amundala <tom.amundala@a2k.africa>
2024-04-09 15:15:20 +0200

    get data from api and new interfaces

---
+++ src/types.ts
@@ -0,0 +1,53 @@
+//=== Creating options interface ===
+
+import { HttpContext, HttpHeaders, HttpParams } from '@angular/common/http';
+
+export interface Options {
+  headers?:
+    | HttpHeaders
+    | {
+        [header: string]: string | string[];
+      };
+  observe?: 'body';
+  context?: HttpContext;
+  params?:
+    | HttpParams
+    | {
+        [param: string]:
+          | string
+          | number
+          | boolean
+          | ReadonlyArray<string | number | boolean>;
+      };
+  reportProgress?: boolean;
+  responseType?: 'json';
+  withCredentials?: boolean;
+  transferCache?:
+    | {
+        includeHeaders?: string[];
+      }
+    | boolean;
+}
+
+export interface Products {
+  item: Product[];
+  total: number;
+  page: number;
+  perPage: number;
+  totalPages: number;
+}
+export interface Product {
+  name: string;
+  price: string;
+  image: string;
+  rating: number;
+}
+export interface PaginationParams {
+  [param: string]:
+    | string
+    | number
+    | boolean
+    | ReadonlyArray<string | number | boolean>;
+  page: number;
+  perPage: number;
+}
