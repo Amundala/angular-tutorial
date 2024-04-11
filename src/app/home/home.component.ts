e9ecd89f82c382388b16a3b9c40b5aebecd62239
Amundala <tom.amundala@a2k.africa>
2024-04-11 15:54:59 +0200

    Adding templates style & Incorporating backend data in UI

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,21 +1,26 @@
 import { Component } from '@angular/core';
 import { ProductsService } from '../services/products.service';
-import { Products } from '../../types';
+import { Product, Products } from '../../types';
+import { ProductComponent } from '../components/product/product.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [ProductComponent, CommonModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss',
 })
 export class HomeComponent {
   constructor(private produductService: ProductsService) {}
+
+  //=== create products array ===
+  products: Product[] = [];
   ngOnInit() {
     this.produductService
       .getProducts('http://localhost:3000/clothes', { page: 0, perPage: 5 })
       .subscribe((products: Products) => {
-        console.log(products.items, 'ITEMSSSS');
+        this.products = products.items;
       });
   }
 }
